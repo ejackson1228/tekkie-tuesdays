dbd0bbceff718dc1bc927fb6f10f82a76f6c714d
ejackson1228 <ejackson1228@gmail.com>
2022-10-14 23:42:00 -0400

    Models (User, Post, Comment, Like) built with references established

--- models/Post.js
+++ models/Post.js
@@ -1,7 +1,30 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Post extends Model {}
+class Post extends Model {
+    static like(body, models) {
+        return models.Like.create({
+            user_id: body.user_id,
+            post_id: body.post_id
+        }).then(() => {
+            return Post.findOne({
+                where: {
+                    id: body.post_id
+                },
+                attributes: [
+                    'id',
+                    'post_url',
+                    'title',
+                    'created_at',
+                    [
+                        sequelize.literal('(SELECT COUNT(*) FROM like WHERE post.id = like.post_id)'),
+                        'like_count'
+                    ]
+                ]
+            });
+        });
+    };
+};
 
 Post.init(
     {
@@ -10,6 +33,36 @@ Post.init(
             allowNull: false,
             primaryKey: true,
             autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        article_url: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                isUrl: true
+            }
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: id
+            }
         }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
     }
-)
+);
+
+module.exports = Post
