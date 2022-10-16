a2423498a227ee43b9ec83996ba44e59945f201d
ejackson1228 <ejackson1228@gmail.com>
2022-10-16 12:36:15 -0400

    created CRUD user routes and session login/out for users

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,96 @@
+const router = require('express').Router();
+const sequelize = require('../../config/connection');
+const { Post, User, Like, Comment } = require('../../models');
+
+// get all posts
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [ //include like count in sequelize literal 
+            'id', 
+            'title', 
+            'text', 
+            'user_id', 
+            'article_url',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM like WHERE post.id = like.post_id)'), 'like_count']
+        ],
+        order: [['created_at', 'DESC']],
+        include: [
+            {
+                model: Comment, //include comments with user who created the comment
+                attributes: ['id', 'text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes:  ['username']
+                }
+            },
+            {
+                model: User, //include author of post
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
+
+//get post by id
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id', 
+            'title', 
+            'text', 
+            'user_id', 
+            'article_url',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM like WHERE post.id = like.post_id)'), 'like_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No Post found with that ID.'});
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
+
+//create new post
+router.post('/', (req, res) => {
+    Post.create({
+        title: req.body.title,
+        text: req.body.text,
+        article_url: req.body.article_url,
+        user_id: req.session.user_id
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -77,9 +77,33 @@ router.post('/', (req, res) => {
 });
 
 //create login session from existing user 
-//router.post('/login', (req, res) => {
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with that email' });
+            return;
+        }
 
-//});
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password'});
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in.'})
+        });
+    });
+});
 
 // change user info
 router.put('/:id', (req, res) => {
@@ -100,6 +124,36 @@ router.put('/:id', (req, res) => {
         console.log(err);
         res.status(500).json(err);
     });
+});
+
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with that ID.'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
+
+//create logout route to terminate session 
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        })
+    } else {
+        res.status(404).end();
+    }
 });
 
 
