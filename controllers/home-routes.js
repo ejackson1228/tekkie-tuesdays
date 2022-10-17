56b9890fe4579e6bddea2781411332b156a3c1b8
ejackson1228 <ejackson1228@gmail.com>
2022-10-17 18:12:05 -0400

    added fully funcional login page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -47,6 +47,49 @@ router.get('/login', (req, res) => {
         return;
     }
     res.render('login');
+});
+
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'article_url',
+            'description',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes:['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with that ID.'});
+            return;
+        }
+
+        const post = dbPostData.get({ plain: true });
+
+        res.render('single-post', {
+            post,
+            loggedIn: req.session.loggedIn
+        });
+    })
 })
 
 module.exports = router;
